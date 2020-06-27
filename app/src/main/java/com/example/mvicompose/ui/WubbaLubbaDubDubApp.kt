a6d9c68d9f2e3f4ce5e888efb877084c04e30c91
Oscar Sequeiros <oscar.sequeiros_ex@scotibank.cl>
2020-06-27 01:37:19 -0400

    Adding Application class.

---
+++ app/src/main/java/com/example/mvicompose/ui/WubbaLubbaDubDubApp.kt
@@ -0,0 +1,10 @@
+package com.example.mvicompose.ui
+
+import android.app.Application
+
+class WubbaLubbaDubDubApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
